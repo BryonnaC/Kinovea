a6e61708feb21cfe8fc13f85d2dfda57b8b8549b
BryonnaC <bryonnacrowley@gmail.com>
2022-11-10 20:31:12 -0500

    getting ducks in rows

--- Analysis-ter/AnalysistemProgram.cs
+++ Analysis-ter/AnalysistemProgram.cs
@@ -13,7 +13,7 @@ namespace Analysistem
      * 
      * this description may change, those last two lines make little sense
      */
-    static class Program    //consider renaming to something more useful? 
+    static class AnalysistemProgram    //consider renaming to something more useful? 
     {
         /// <summary>
         /// The main entry point for the application.
