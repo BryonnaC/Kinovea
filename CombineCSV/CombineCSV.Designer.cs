f08fce726f7e2e098babac1cd8e77c1fea7f1424
BryonnaC <bryonnacrowley@gmail.com>
2022-11-26 17:22:46 -0500

    fix

--- CombineCSV/CombineCSV.Designer.cs
+++ CombineCSV/CombineCSV.Designer.cs
@@ -64,9 +64,10 @@ namespace CombineCSV
             this.panel1.Controls.Add(this.panel2);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Padding = new System.Windows.Forms.Padding(10, 9, 10, 9);
-            this.panel1.Size = new System.Drawing.Size(1236, 315);
+            this.panel1.Padding = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.panel1.Size = new System.Drawing.Size(824, 205);
             this.panel1.TabIndex = 0;
             // 
             // panel2
@@ -75,9 +76,10 @@ namespace CombineCSV
             this.panel2.Controls.Add(this.panel3);
             this.panel2.Controls.Add(this.tableLayoutPanel1);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel2.Location = new System.Drawing.Point(10, 9);
+            this.panel2.Location = new System.Drawing.Point(7, 6);
+            this.panel2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(1216, 297);
+            this.panel2.Size = new System.Drawing.Size(810, 193);
             this.panel2.TabIndex = 0;
             // 
             // panel3
@@ -87,10 +89,11 @@ namespace CombineCSV
             this.panel3.Controls.Add(this.btnCombine);
             this.panel3.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.panel3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.panel3.Location = new System.Drawing.Point(0, 229);
+            this.panel3.Location = new System.Drawing.Point(0, 149);
+            this.panel3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel3.Name = "panel3";
-            this.panel3.Padding = new System.Windows.Forms.Padding(10, 9, 10, 9);
-            this.panel3.Size = new System.Drawing.Size(1216, 68);
+            this.panel3.Padding = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.panel3.Size = new System.Drawing.Size(810, 44);
             this.panel3.TabIndex = 2;
             // 
             // lblUserMessage
@@ -98,9 +101,10 @@ namespace CombineCSV
             this.lblUserMessage.AutoSize = true;
             this.lblUserMessage.Font = new System.Drawing.Font("Bahnschrift", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblUserMessage.ForeColor = System.Drawing.SystemColors.Control;
-            this.lblUserMessage.Location = new System.Drawing.Point(216, 22);
+            this.lblUserMessage.Location = new System.Drawing.Point(144, 14);
+            this.lblUserMessage.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblUserMessage.Name = "lblUserMessage";
-            this.lblUserMessage.Size = new System.Drawing.Size(0, 27);
+            this.lblUserMessage.Size = new System.Drawing.Size(0, 18);
             this.lblUserMessage.TabIndex = 3;
             // 
             // btnCombine
@@ -110,9 +114,10 @@ namespace CombineCSV
             this.btnCombine.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnCombine.Font = new System.Drawing.Font("Bahnschrift", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnCombine.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.btnCombine.Location = new System.Drawing.Point(10, 9);
+            this.btnCombine.Location = new System.Drawing.Point(7, 6);
+            this.btnCombine.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnCombine.Name = "btnCombine";
-            this.btnCombine.Size = new System.Drawing.Size(200, 50);
+            this.btnCombine.Size = new System.Drawing.Size(133, 32);
             this.btnCombine.TabIndex = 2;
             this.btnCombine.Text = "Combine";
             this.btnCombine.UseVisualStyleBackColor = false;
@@ -125,12 +130,13 @@ namespace CombineCSV
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 0, 0);
             this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel2, 0, 1);
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(2, 2);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 2;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(1214, 225);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(809, 146);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
             // flowLayoutPanel1
@@ -139,10 +145,10 @@ namespace CombineCSV
             this.flowLayoutPanel1.Controls.Add(this.tableLayoutPanel2);
             this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(10, 9);
-            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(10, 9, 10, 9);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(7, 6);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(1194, 94);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(795, 61);
             this.flowLayoutPanel1.TabIndex = 0;
             // 
             // lblXPos
@@ -150,10 +156,11 @@ namespace CombineCSV
             this.lblXPos.AutoSize = true;
             this.lblXPos.Font = new System.Drawing.Font("Bahnschrift", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblXPos.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.lblXPos.Location = new System.Drawing.Point(3, 0);
-            this.lblXPos.MinimumSize = new System.Drawing.Size(80, 29);
+            this.lblXPos.Location = new System.Drawing.Point(2, 0);
+            this.lblXPos.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblXPos.MinimumSize = new System.Drawing.Size(53, 19);
             this.lblXPos.Name = "lblXPos";
-            this.lblXPos.Size = new System.Drawing.Size(255, 29);
+            this.lblXPos.Size = new System.Drawing.Size(173, 19);
             this.lblXPos.TabIndex = 0;
             this.lblXPos.Text = "Path to X positions file:";
             this.toolTip.SetToolTip(this.lblXPos, resources.GetString("lblXPos.ToolTip"));
@@ -165,23 +172,24 @@ namespace CombineCSV
             this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 5.856833F));
             this.tableLayoutPanel2.Controls.Add(this.txtXPos, 0, 0);
             this.tableLayoutPanel2.Controls.Add(this.btnPasteXPos, 1, 0);
-            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 32);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(2, 21);
+            this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tableLayoutPanel2.Name = "tableLayoutPanel2";
-            this.tableLayoutPanel2.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.tableLayoutPanel2.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
             this.tableLayoutPanel2.RowCount = 1;
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 47F));
-            this.tableLayoutPanel2.Size = new System.Drawing.Size(1190, 57);
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 31F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(793, 37);
             this.tableLayoutPanel2.TabIndex = 2;
             // 
             // txtXPos
             // 
             this.txtXPos.Dock = System.Windows.Forms.DockStyle.Fill;
             this.txtXPos.Font = new System.Drawing.Font("Bahnschrift", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtXPos.Location = new System.Drawing.Point(4, 10);
-            this.txtXPos.Margin = new System.Windows.Forms.Padding(0, 5, 0, 5);
+            this.txtXPos.Location = new System.Drawing.Point(3, 6);
+            this.txtXPos.Margin = new System.Windows.Forms.Padding(0, 3, 0, 3);
             this.txtXPos.Name = "txtXPos";
-            this.txtXPos.Size = new System.Drawing.Size(1112, 36);
+            this.txtXPos.Size = new System.Drawing.Size(740, 27);
             this.txtXPos.TabIndex = 0;
             // 
             // btnPasteXPos
@@ -192,10 +200,10 @@ namespace CombineCSV
             this.btnPasteXPos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnPasteXPos.Font = new System.Drawing.Font("Bahnschrift", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnPasteXPos.Image = global::CombineCSV.Properties.Resources.paste_32;
-            this.btnPasteXPos.Location = new System.Drawing.Point(1126, 5);
-            this.btnPasteXPos.Margin = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.btnPasteXPos.Location = new System.Drawing.Point(750, 3);
+            this.btnPasteXPos.Margin = new System.Windows.Forms.Padding(7, 0, 0, 0);
             this.btnPasteXPos.Name = "btnPasteXPos";
-            this.btnPasteXPos.Size = new System.Drawing.Size(60, 47);
+            this.btnPasteXPos.Size = new System.Drawing.Size(40, 31);
             this.btnPasteXPos.TabIndex = 2;
             this.btnPasteXPos.TabStop = false;
             this.toolTip.SetToolTip(this.btnPasteXPos, "Paste path from clipboard");
@@ -208,10 +216,10 @@ namespace CombineCSV
             this.flowLayoutPanel2.Controls.Add(this.tableLayoutPanel3);
             this.flowLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.flowLayoutPanel2.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
-            this.flowLayoutPanel2.Location = new System.Drawing.Point(10, 121);
-            this.flowLayoutPanel2.Margin = new System.Windows.Forms.Padding(10, 9, 10, 9);
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(7, 79);
+            this.flowLayoutPanel2.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
             this.flowLayoutPanel2.Name = "flowLayoutPanel2";
-            this.flowLayoutPanel2.Size = new System.Drawing.Size(1194, 95);
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(795, 61);
             this.flowLayoutPanel2.TabIndex = 2;
             // 
             // lblYPos
@@ -219,10 +227,11 @@ namespace CombineCSV
             this.lblYPos.AutoSize = true;
             this.lblYPos.Font = new System.Drawing.Font("Bahnschrift", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblYPos.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.lblYPos.Location = new System.Drawing.Point(3, 0);
-            this.lblYPos.MinimumSize = new System.Drawing.Size(80, 29);
+            this.lblYPos.Location = new System.Drawing.Point(2, 0);
+            this.lblYPos.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblYPos.MinimumSize = new System.Drawing.Size(53, 19);
             this.lblYPos.Name = "lblYPos";
-            this.lblYPos.Size = new System.Drawing.Size(253, 29);
+            this.lblYPos.Size = new System.Drawing.Size(172, 19);
             this.lblYPos.TabIndex = 0;
             this.lblYPos.Text = "Path to Y positions file:";
             this.toolTip.SetToolTip(this.lblYPos, resources.GetString("lblYPos.ToolTip"));
@@ -234,22 +243,23 @@ namespace CombineCSV
             this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 5.856833F));
             this.tableLayoutPanel3.Controls.Add(this.txtYPos, 0, 0);
             this.tableLayoutPanel3.Controls.Add(this.btnPasteYPos, 1, 0);
-            this.tableLayoutPanel3.Location = new System.Drawing.Point(3, 32);
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(2, 21);
+            this.tableLayoutPanel3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tableLayoutPanel3.Name = "tableLayoutPanel3";
-            this.tableLayoutPanel3.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.tableLayoutPanel3.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
             this.tableLayoutPanel3.RowCount = 1;
             this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel3.Size = new System.Drawing.Size(1185, 55);
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(790, 36);
             this.tableLayoutPanel3.TabIndex = 3;
             // 
             // txtYPos
             // 
             this.txtYPos.Dock = System.Windows.Forms.DockStyle.Fill;
             this.txtYPos.Font = new System.Drawing.Font("Bahnschrift", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtYPos.Location = new System.Drawing.Point(4, 10);
-            this.txtYPos.Margin = new System.Windows.Forms.Padding(0, 5, 0, 5);
+            this.txtYPos.Location = new System.Drawing.Point(3, 6);
+            this.txtYPos.Margin = new System.Windows.Forms.Padding(0, 3, 0, 3);
             this.txtYPos.Name = "txtYPos";
-            this.txtYPos.Size = new System.Drawing.Size(1108, 36);
+            this.txtYPos.Size = new System.Drawing.Size(738, 27);
             this.txtYPos.TabIndex = 1;
             // 
             // btnPasteYPos
@@ -259,10 +269,10 @@ namespace CombineCSV
             this.btnPasteYPos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnPasteYPos.Font = new System.Drawing.Font("Bahnschrift", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnPasteYPos.Image = global::CombineCSV.Properties.Resources.paste_32;
-            this.btnPasteYPos.Location = new System.Drawing.Point(1122, 5);
-            this.btnPasteYPos.Margin = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.btnPasteYPos.Location = new System.Drawing.Point(748, 3);
+            this.btnPasteYPos.Margin = new System.Windows.Forms.Padding(7, 0, 0, 0);
             this.btnPasteYPos.Name = "btnPasteYPos";
-            this.btnPasteYPos.Size = new System.Drawing.Size(59, 45);
+            this.btnPasteYPos.Size = new System.Drawing.Size(39, 30);
             this.btnPasteYPos.TabIndex = 2;
             this.btnPasteYPos.TabStop = false;
             this.toolTip.SetToolTip(this.btnPasteYPos, "Paste path from clipboard");
@@ -272,13 +282,14 @@ namespace CombineCSV
             // CombineCSV
             // 
             this.AcceptButton = this.btnCombine;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
-            this.ClientSize = new System.Drawing.Size(1236, 315);
+            this.ClientSize = new System.Drawing.Size(824, 205);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.MinimumSize = new System.Drawing.Size(97, 91);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.MinimumSize = new System.Drawing.Size(70, 73);
             this.Name = "CombineCSV";
             this.Text = "CombineCSV";
             this.panel1.ResumeLayout(false);
