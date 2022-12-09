7e95ba6c26a613b3a98a0808b2d09946f2d45063
ghgsrt <ajb7366@psu.edu>
2022-12-08 20:55:14 -0500

    Colin do something with this thing

--- Analysis-ter/Time.cs
+++ Analysis-ter/Time.cs
@@ -16,6 +16,17 @@ namespace Analysistem.Utils
             return elapsed.Ticks / (double)TimeSpan.TicksPerMillisecond * (int)unit;
         }
 
+        public static double ToFromUnits(this double time, Unit oldUnits, Unit newUnits)
+        {
+            switch(oldUnits)
+            {
+                case Unit.Milliseconds: return time * (double) newUnits;
+                case Unit.Microseconds: return oldUnits < newUnits ? time * 1_000 : time / 1_000;
+                case Unit.Nanoseconds: return newUnits == Unit.Microseconds ? time / 1_000 : time / 1_000_000;
+                default: return time; // this will never be reached
+            }
+        }
+
         public static string GetTimestamp(this DateTime value)
         {
             return value.ToString("yyyy-MM-dd HH:mm:ss:ffff");
